e7df29ad8ab3c09cd15274d2d363bf9fb28092fd
Stepan Kryukov <stephenkryukov@gmail.com>
2022-09-09 22:48:09 +0300

    navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,22 @@
 import Searchbar from './Searchbar/Searchbar';
+import Navigation from './Navigation/Navigation';
 
 export const App = () => {
   return (
-    <div
-    // style={{
-    //   height: '100vh',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   fontSize: 40,
-    //   color: '#010101',
-    // }}
-    >
-      <Searchbar />
-    </div>
+    <>
+      <div
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101',
+      // }}
+      >
+        <Searchbar />
+        <Navigation />
+      </div>
+    </>
   );
 };
